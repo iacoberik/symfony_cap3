d1dbdda5f08fac7bc9b09238ba8d84fd8eb66b6c
iacoberik <iacob.erik320@gmail.com>
2023-02-16 21:22:52 +0200

    ??

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -42,7 +42,7 @@ class VinylController extends AbstractController
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
         $mixes = $mixRepository->findAll();
-        
+
         return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
             'mixes' => $mixes,
